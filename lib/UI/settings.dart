4c4eba3232b5a1497b3177c86d16444e6d103ffe
Youssef Mohamed <79748625+YoussefMohamed7557@users.noreply.github.com>
2024-06-23 22:01:37 +0300

    finish the design of settings tap

--- lib/UI/settings.dart
+++ lib/UI/settings.dart
@@ -5,6 +5,56 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.transparent,);
+    return Column(
+      children: [
+        Container(height: 80,color: Theme.of(context).primaryColor,),
+        Spacer(),
+        Padding(padding: EdgeInsets.all(16.0),
+        child:Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("language",style: Theme.of(context).textTheme.displayMedium?.copyWith(color:Colors.black87),),
+            InkWell(
+              onTap: (){
+                //showLanguageBottomSheet();
+              },
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("English", style: TextStyle(fontSize: 22,fontWeight: FontWeight.w400,color:Colors.black87),),
+                    Icon(Icons.arrow_drop_down_circle_outlined,size: 28,color: Colors.black54,)
+                  ],
+                ),
+
+                margin: EdgeInsets.all(12),
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(color:Colors.white,borderRadius: BorderRadius.circular(12),border: Border.all(color:Colors.black87)),
+              ),
+            ),
+            Text("Mode",style: Theme.of(context).textTheme.displayMedium?.copyWith(color:Colors.black87),),
+            InkWell(
+              onTap: (){
+                //showThemeBottomSheet();
+              },
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("light mode", style: TextStyle(fontSize: 22,fontWeight: FontWeight.w400,color: Colors.black87),),
+                    Icon(Icons.arrow_drop_down_circle_outlined,size: 30,color: Colors.black54,)
+                  ],
+                ),
+                margin: EdgeInsets.all(12),
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(color:Colors.white,borderRadius: BorderRadius.circular(12),border: Border.all(color: Colors.black87)),
+              ),
+            )
+          ],
+        ), ),
+        Spacer(flex: 3,),
+      ],
+    );
   }
 }
